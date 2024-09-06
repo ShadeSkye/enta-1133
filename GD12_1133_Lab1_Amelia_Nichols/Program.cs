880b3ac7828acb8fcb57ec8d0e750253f97cbd4c
ShadeSkye <amelia6nichols@gmail.com>
2024-09-06 12:12:09 -0700

    Exploring Visual Studio
    Basic project with an extra line

---
+++ GD12_1133_Lab1_Amelia_Nichols/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_Amelia_Nichols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I'm a goofy goober!");
+        }
+    }
+}
