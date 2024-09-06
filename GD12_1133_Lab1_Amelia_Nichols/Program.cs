b9b567cc1c1485b07fb292ed33537654337f927c
ShadeSkye <amelia6nichols@gmail.com>
2024-09-06 14:50:56 -0700

    Completed Lab Assignment 1

--- GD12_1133_Lab1_Amelia_Nichols/Program.cs
+++ GD12_1133_Lab1_Amelia_Nichols/Program.cs
@@ -1,11 +1,36 @@
-﻿namespace GD12_1133_Lab1_Amelia_Nichols
+﻿using System;
+
+namespace GD12_1133_Lab1_Amelia_Nichols
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("I'm a goofy goober!");
+            Console.WriteLine("I've been expecting you.");
+           
+
+            Console.WriteLine("decimal: 0, binary: 0 hexadecimal: 0");
+
+            Console.WriteLine("decimal: 1, binary: 1, hexadecimal: 1");
+
+            Console.WriteLine("decimal: 2, binary: 1 0, hexadecimal: 2");
+
+            Console.WriteLine("decimal: 4, binary: 1 0 0, hexadecimal: 4");
+
+            Console.WriteLine("decimal: 8, binary: 1 0 0 0, hexadecimal: 8");
+
+            Console.WriteLine("decimal: 16, binary: 1 0 0 0 0, hexadecimal: 10");
+
+            Console.WriteLine("decimal: 32, binary: 1 0 0 0 0 0, hexadecimal: 20");
+
+            Console.WriteLine("decimal: 64, binary: 1 0 0 0 0 0 0, hexadecimal: 40");
+
+            Console.WriteLine("decimal: 100, binary: 1 1 0 0 1 0 0, hexadecimal: 64");
+
+            Console.WriteLine("decimal: 255, binary: 1 1 1 1 1 1 1 1, hexadecimal: FF");
+
+
+            Console.WriteLine("Amelia Nichols, 09/06/24");
         }
     }
 }
