73fa5205eda21be3f69e6d35d07cb4f81a260183
ShadeSkye <amelia6nichols@gmail.com>
2024-09-19 10:12:25 -0700

    Lab2 Die Roller
    I created a die roller using C#

---
+++ AmeliaNLab2/DieRoller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeliaNLab2
+{
+    internal class DieRoller
+    {
+        int minRoll = 1;    //highest and lowest sides of the die
+        int maxRoll = 6;
+        public int side = 0;
+
+
+        /// <summary>
+        /// the roll function generates a random number from "minRoll" to "maxRoll" then prints the side you landed on for you to see
+        /// </summary>
+        internal void roll()
+        {
+            Random randomSide;
+            randomSide = new Random();
+
+            side = randomSide.Next(minRoll, maxRoll);
+
+            Console.WriteLine(side + "!");  //represents what side the die landed on
+            Console.WriteLine("");
+            
+        }
+
+        
+
+
+
+
+
+
+
+    }
+}
