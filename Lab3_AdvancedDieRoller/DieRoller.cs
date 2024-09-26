71eebaf62204b57e2ad1e9307e1b3c6a8747bc1e
ShadeSkye <amelia6nichols@gmail.com>
2024-09-26 13:05:44 -0700

    Finished part 1 of the advanced die roller

--- Lab3_AdvancedDieRoller/DieRoller.cs
+++ Lab3_AdvancedDieRoller/DieRoller.cs
@@ -8,23 +8,62 @@ namespace Lab3_AdvancedDieRoller
 {
     internal class DieRoller
     {
-        public int ComputerDie = 0;
+        private int ComputerDie = 0;
+        public int ComputerDieResult = 0;
+        public int PlayerDieResult = 0;
+        private int MaxPlayerRoll = 0;
+        private int MaxComputerRoll = 0;
+
+        internal void StartRolling()
+        {
+            Player PlayerInstance = new Player();
+            PlayerInstance.PickPlayerDie();
+            MaxPlayerRoll = PlayerInstance.MaxRoll;
+            ComputerPickDie();
+            RollPlayerDie();
+            RollComputerDie();
+        }
 
         internal void ComputerPickDie()
         {
-            Random RandomDie = new Random();
-            ComputerDie = RandomDie.Next(1, 5);
+            Random RandomComputerDie = new Random();
+            ComputerDie = RandomComputerDie.Next(1, 5);
 
+            if (ComputerDie == 1)
+            {
+                MaxComputerRoll = 7;
+            }
 
+            else if (ComputerDie == 2)
+            {
+                MaxComputerRoll = 9;
+            }
 
+            else if (ComputerDie == 3)
+            {
+                MaxComputerRoll = 13;
+            }
+
+            else if (ComputerDie == 4)
+            {
+                MaxComputerRoll = 21;
+            }
         }
 
         internal void RollPlayerDie()
         {
-
-
-
+            Random RandomPlayerDieResult = new Random();
+            PlayerDieResult = RandomPlayerDieResult.Next(1, MaxPlayerRoll);
+            Console.WriteLine("\nYou rolled a " + PlayerDieResult + ".");
         }
+
+        internal void RollComputerDie()
+        {
+            Random RandomComputerDieResult = new Random();
+            ComputerDieResult = RandomComputerDieResult.Next(1, MaxComputerRoll);
+            Console.WriteLine("\nI rolled a " + ComputerDieResult + ".");
+        }
+
 
 
 
