9279216142d62566e07f2934a9f5eb11d8851be6
ShadeSkye <amelia6nichols@gmail.com>
2024-10-08 17:09:20 -0700

    problem fix

---
+++ DungeonDicer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonDicer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
